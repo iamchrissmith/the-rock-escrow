b52ede0ec171f1b3245cab45a29dc3c9d76515bb
Chris Smith <42@chrisryansmith.com>
2017-07-14 21:58:12 -0600

    hook contract to React

--- src/App.js
+++ src/App.js
@@ -10,6 +10,187 @@ const WEB3 = new Web3()
 
 const ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 
+const ROCK_ABI = [
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "interactions",
+    "outputs": [
+      {
+        "name": "player1",
+        "type": "address"
+      },
+      {
+        "name": "player2",
+        "type": "address"
+      },
+      {
+        "name": "player2FailureCount",
+        "type": "uint256"
+      },
+      {
+        "name": "player1FailureCount",
+        "type": "uint256"
+      },
+      {
+        "name": "periodLength",
+        "type": "uint256"
+      },
+      {
+        "name": "periodCount",
+        "type": "uint256"
+      },
+      {
+        "name": "periodOn",
+        "type": "uint256"
+      },
+      {
+        "name": "amountLost",
+        "type": "uint256"
+      },
+      {
+        "name": "playerAmount",
+        "type": "uint256"
+      },
+      {
+        "name": "charity",
+        "type": "address"
+      },
+      {
+        "name": "interactionOver",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_player1Result",
+        "type": "bool"
+      },
+      {
+        "name": "_player2Result",
+        "type": "bool"
+      }
+    ],
+    "name": "nextPeriod",
+    "outputs": [],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "periodLength",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getFailureCounts",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getInteractionResults",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool[]"
+      },
+      {
+        "name": "",
+        "type": "bool[]"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_playerAmount",
+        "type": "uint256"
+      },
+      {
+        "name": "_player1",
+        "type": "address"
+      },
+      {
+        "name": "_player2",
+        "type": "address"
+      },
+      {
+        "name": "_periodLength",
+        "type": "uint256"
+      },
+      {
+        "name": "_periodCount",
+        "type": "uint256"
+      },
+      {
+        "name": "_charity",
+        "type": "address"
+      }
+    ],
+    "name": "startInteraction",
+    "outputs": [],
+    "payable": true,
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "type": "constructor"
+  }
+]
+
+const ROCK_ADDRESS = '0xac2ddc306be4db9408e2c7b4f86417e6655c434c'
+//
+const ROCK_APP = ETHEREUM_CLIENT.eth.contract(ROCK_ABI).at(ROCK_ADDRESS)
+
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,22 +198,31 @@ class App extends Component {
       view: 'main',
     };
     this._onRockClick = this._onRockClick.bind(this);
+    this._onCandyAssClick = this._onCandyAssClick.bind(this);
+    this._onMainClick = this._onMainClick.bind(this);
   }
 
   componentDidMount() {
-    this._setupAccount();
+    console.log(ROCK_APP.owner())
+    this._setupAccount()
+    this.startInteraction()
+    this.getInteractionResults()
   }
 
   _setupAccount() {
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
 
     this.setState({
-      account: ETHEREUM_CLIENT.toChecksumAddress(ETHEREUM_CLIENT.eth.coinbase),
+      account: ETHEREUM_CLIENT.toChecksumAddress(ETHEREUM_CLIENT.eth.accounts[0]),
       balance: ETHEREUM_CLIENT.fromWei( (ETHEREUM_CLIENT.eth.getBalance(ETHEREUM_CLIENT.eth.accounts[0])), 'ether') .toString()
-      // account: Web3.toChecksumAddress(Web3.eth.coinbase)
     })
   }
 
+  _onMainClick() {
+    this.setState({
+      view: 'main',
+    });
+  }
 
   _onRockClick() {
     this.setState({
@@ -40,7 +230,38 @@ class App extends Component {
     });
   }
 
+  _onCandyAssClick() {
+    this.setState({
+      view: 'candyass',
+    });
+  }
+
+  async startInteraction() {
+    let playerAmount = 100;
+    let player1 = ETHEREUM_CLIENT.eth.accounts[1];
+    let player2 = ETHEREUM_CLIENT.eth.accounts[2];
+    let periodLength = 0;
+    let periodCount = 52;
+    let charity = ETHEREUM_CLIENT.eth.accounts[3];
+    console.log(charity);
+    await ROCK_APP.startInteraction(playerAmount, player1, player2, periodLength, periodCount, charity, {from: ETHEREUM_CLIENT.eth.accounts[0], value: 20*10**18});
+  }
+
+  getInteractionResults() {
+    let results = ROCK_APP.getInteractionResults()
+    console.log(results)
+    return results
+  }
+
+  nextPeriod() {}
+
   render() {
+    let this_view;
+    if (this.state.view === 'rock') {
+      this_view = <TheRock />
+    } else if (this.state.view === 'candyass') {
+      this_view = <TheCandyAss />
+    }
     return (
       <div>
       <div className="App">
@@ -50,13 +271,10 @@ class App extends Component {
         </div>
         <p>Your account: {this.state.account}</p>
         <p>Your balance: {this.state.balance}</p>
-        { this.state.view == 'main' ?
-
-            <button onClick={this._onRockClick}>The Rock</button>
-            // <TheCandyAss />
-        :
-          <TheRock />
-        }
+        <button onClick={this._onMainClick}>Home</button>
+        <button onClick={this._onRockClick}>The Rock</button>
+        <button onClick={this._onCandyAssClick}>CandyAss</button>
+        { this_view }
         </div>
       </div>
     );
